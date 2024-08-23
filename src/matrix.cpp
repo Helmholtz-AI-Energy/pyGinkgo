194c6562f1f39dde013013684d128de735c8fc11
Roman Mishchuk <romamiykg@gmail.com>
2024-08-24 01:54:22 +0300

    Indentations to tabs

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -9,88 +9,96 @@ namespace py = pybind11;
 template <class MatrixName>
 void gko_matrix_binding(py::module_ &module_matrix, const char name[])
 {
-  py::class_<MatrixName,
-             std::shared_ptr<MatrixName>, gko::LinOp>(
-      module_matrix, name, py::buffer_protocol())
-      .def(py::init([](std::shared_ptr<gko::Executor> exec) {
-        return MatrixName::create(exec);
-      }))
-      .def(py::init([](std::shared_ptr<gko::Executor> exec, py::tuple dim,
-                       gko::array<ValueType> &vals,
-                       gko::array<IndexType> &cols,
-                       gko::array<IndexType> &rows) {
-        return gko::share(MatrixName::create(
-            exec, gko::dim<2>{dim[0].cast<size_t>(), dim[1].cast<size_t>()},
-            vals, cols, rows));
-      }))
-      .def(py::init([](std::shared_ptr<gko::Executor> exec, py::tuple dim,
-                       py::buffer data, py::buffer rows, py::buffer cols) {
-        /* Request a buffer descriptor from Python */
-        py::buffer_info data_info = data.request();
-        py::buffer_info rows_info = rows.request();
-        py::buffer_info cols_info = cols.request();
+	py::class_<MatrixName, std::shared_ptr<MatrixName>, gko::LinOp>(
+		module_matrix, name, py::buffer_protocol()
+	)
+		.def(py::init([](std::shared_ptr<gko::Executor> exec) {
+			return MatrixName::create(exec);
+		}))
 
-        if (data_info.format != py::format_descriptor<ValueType>::format())
-          throw std::runtime_error(
-              "Provided values have an incompatible dtype");
+		.def(py::init(
+			[](std::shared_ptr<gko::Executor> exec, py::tuple dim,
+				gko::array<ValueType> &vals, gko::array<IndexType> &cols,
+				gko::array<IndexType> &rows) 
+			{
+				return gko::share(MatrixName::create(
+					exec, gko::dim<2>{dim[0].cast<size_t>(), dim[1].cast<size_t>()},
+					vals, cols, rows)
+				);
+			}))
+		
+		.def(py::init(
+			[](std::shared_ptr<gko::Executor> exec, py::tuple dim,
+				py::buffer data, py::buffer rows, py::buffer cols) 
+			{
+				/* Request a buffer descriptor from Python */
+				py::buffer_info data_info = data.request();
+				py::buffer_info rows_info = rows.request();
+				py::buffer_info cols_info = cols.request();
 
-        if (rows_info.format != py::format_descriptor<IndexType>::format())
-          throw std::runtime_error(
-              "Provided rows have an incompatible dtype");
+				if (data_info.format != py::format_descriptor<ValueType>::format())
+					throw std::runtime_error(
+						"Provided values have an incompatible dtype");
 
-        if (cols_info.format != py::format_descriptor<IndexType>::format())
-          throw std::runtime_error(
-              "Provided cols have an incompatible dtype");
+				if (rows_info.format != py::format_descriptor<IndexType>::format())
+					throw std::runtime_error(
+						"Provided rows have an incompatible dtype");
 
-        auto ref = gko::ReferenceExecutor::create();
+				if (cols_info.format != py::format_descriptor<IndexType>::format())
+					throw std::runtime_error(
+						"Provided cols have an incompatible dtype");
 
-        /* create a view into numpy data */
-        auto nnz = data_info.shape[0];
+				auto ref = gko::ReferenceExecutor::create();
 
-        auto data_view =
-            gko::array<ValueType>::view(ref, nnz, (ValueType *)data_info.ptr);
-        auto rows_view =
-            gko::array<IndexType>::view(ref, nnz, (IndexType *)rows_info.ptr);
-        auto cols_view =
-            gko::array<IndexType>::view(ref, nnz, (IndexType *)cols_info.ptr);
+				/* create a view into numpy data */
+				auto nnz = data_info.shape[0];
 
-        return gko::share(MatrixName::create(
-            exec, gko::dim<2>{dim[0].cast<size_t>(), dim[1].cast<size_t>()},
-            data_view, cols_view, rows_view));
-      }))
-      .def(
-          "apply",
-          [](const MatrixName &m,
-             std::shared_ptr<const gko::LinOp> b,
-             std::shared_ptr<gko::LinOp> x) { m.apply(b, x); },
-          "")
-      .def("__repr__",
-           [](const MatrixName &o) {
-             auto str = std::string("pygko.matrix.Name object");
-             return str;
-           })
-      .def("get_num_stored_elements",
-           &MatrixName::get_num_stored_elements,
-           "Applies Name matrix axpy to a vector (or a sequence of vectors)."
-           "Performs the operation x = Name * b + x");
+				auto data_view = 
+						gko::array<ValueType>::view(ref, nnz, (ValueType *)data_info.ptr);
+				auto rows_view =
+						gko::array<IndexType>::view(ref, nnz, (IndexType *)rows_info.ptr);
+				auto cols_view =
+						gko::array<IndexType>::view(ref, nnz, (IndexType *)cols_info.ptr);
+
+				return gko::share(MatrixName::create(
+						exec, gko::dim<2>{dim[0].cast<size_t>(), dim[1].cast<size_t>()},
+						data_view, cols_view, rows_view));
+			}))
+		
+		.def("apply",
+			[](const MatrixName &m,
+				std::shared_ptr<const gko::LinOp> b,
+				std::shared_ptr<gko::LinOp> x)
+			{ m.apply(b, x); })
+
+		.def("__repr__",
+			[](const MatrixName &o) {
+				auto str = std::string("pygko.matrix.Name object");
+				return str;
+			})
+		
+		.def("get_num_stored_elements",
+			&MatrixName::get_num_stored_elements,
+			"Applies Name matrix axpy to a vector (or a sequence of vectors)."
+			"Performs the operation x = Name * b + x");
 }
 
 void init_coo(py::module_ &module_matrix) {
-  module_matrix.def("read_Coo", [](const std::string &fn,
-                                   std::shared_ptr<gko::Executor> exec) {
-    return gko::share(
-        gko::read<gko::matrix::Coo<ValueType>>(std::ifstream(fn), exec));
-  });
+	module_matrix.def("read_Coo",
+		[](const std::string &fn, std::shared_ptr<gko::Executor> exec) {
+			return gko::share(
+				gko::read<gko::matrix::Coo<ValueType>>(std::ifstream(fn), exec));
+		});
 
-  gko_matrix_binding<gko::matrix::Coo<ValueType, IndexType>>(module_matrix, "Coo");
+	gko_matrix_binding<gko::matrix::Coo<ValueType, IndexType>>(module_matrix, "Coo");
 }
 
 void init_csr(py::module_ &module_matrix) {
-  module_matrix.def("read_Csr", [](const std::string &fn,
-                                   std::shared_ptr<gko::Executor> exec) {
-    return gko::share(
-        gko::read<gko::matrix::Csr<ValueType>>(std::ifstream(fn), exec));
-  });
+	module_matrix.def("read_Csr",
+		[](const std::string &fn, std::shared_ptr<gko::Executor> exec) {
+			return gko::share(
+				gko::read<gko::matrix::Csr<ValueType>>(std::ifstream(fn), exec));
+		});
 
-  gko_matrix_binding<gko::matrix::Csr<ValueType, IndexType>>(module_matrix, "Csr");
+	gko_matrix_binding<gko::matrix::Csr<ValueType, IndexType>>(module_matrix, "Csr");
 }
--- src/python.cpp
+++ src/python.cpp
@@ -12,41 +12,41 @@ void init_coo(py::module_ &);
 void init_csr(py::module_ &);
 
 PYBIND11_MODULE(pyGinkgo, m) {
-  m.doc() = "Python bindings for the Ginkgo framework";
+	m.doc() = "Python bindings for the Ginkgo framework";
 
-  py::class_<gko::LinOp, std::shared_ptr<gko::LinOp>>(m, "LinOp");
+	py::class_<gko::LinOp, std::shared_ptr<gko::LinOp>>(m, "LinOp");
 
-  py::class_<gko::ptr_param<gko::LinOp>>(m, "ptr_param");
+	py::class_<gko::ptr_param<gko::LinOp>>(m, "ptr_param");
 
-  py::class_<gko::dim<2>>(m, "dim2").def(
-      py::init<gko::size_type, gko::size_type>());
+	py::class_<gko::dim<2>>(m, "dim2")
+		.def(py::init<gko::size_type, gko::size_type>());
 
-  // Docs on usage of shared_ptr here:
-  //    https://pybind11.readthedocs.io/en/stable/advanced/smart_ptrs.html#std-shared-ptr
-  py::class_<gko::Executor, std::shared_ptr<gko::Executor>>(m, "Executor");
+	// Docs on usage of shared_ptr here:
+	//		https://pybind11.readthedocs.io/en/stable/advanced/smart_ptrs.html#std-shared-ptr
+	py::class_<gko::Executor, std::shared_ptr<gko::Executor>>(m, "Executor");
 
-  // Docs on usage of several types here (virtual functions override):
-  //    https://pybind11.readthedocs.io/en/stable/advanced/classes.html#overriding-virtual-functions-in-python
-  py::class_<gko::detail::ExecutorBase<gko::OmpExecutor>, gko::Executor,
-             std::shared_ptr<gko::detail::ExecutorBase<gko::OmpExecutor>>>(
-      m, "OmpExecutorBase");
+	// Docs on usage of several types here (virtual functions override):
+	//		https://pybind11.readthedocs.io/en/stable/advanced/classes.html#overriding-virtual-functions-in-python
+	py::class_<gko::detail::ExecutorBase<gko::OmpExecutor>, gko::Executor,
+			std::shared_ptr<gko::detail::ExecutorBase<gko::OmpExecutor>>>
+		(m, "OmpExecutorBase");
 
-  py::class_<gko::OmpExecutor, gko::detail::ExecutorBase<gko::OmpExecutor>,
-             std::shared_ptr<gko::OmpExecutor>>(m, "OmpExecutor")
-      .def(py::init([]() { return gko::OmpExecutor::create(); }));
+	py::class_<gko::OmpExecutor, gko::detail::ExecutorBase<gko::OmpExecutor>,
+			std::shared_ptr<gko::OmpExecutor>>(m, "OmpExecutor")
+		.def(py::init([]() { return gko::OmpExecutor::create(); }));
 
-  py::class_<gko::ReferenceExecutor, gko::OmpExecutor,
-             std::shared_ptr<gko::ReferenceExecutor>>(m, "ReferenceExecutor")
-      .def(py::init([]() { return gko::ReferenceExecutor::create(); }));
+	py::class_<gko::ReferenceExecutor, gko::OmpExecutor,
+			std::shared_ptr<gko::ReferenceExecutor>>(m, "ReferenceExecutor")
+		.def(py::init([]() { return gko::ReferenceExecutor::create(); }));
 
-  py::module_ module_base =
-      m.def_submodule("base", "Submodule for Ginkgos low level type bindings");
-  init_array(module_base);
+	py::module_ module_base =
+		m.def_submodule("base", "Submodule for Ginkgos low level type bindings");
+	init_array(module_base);
 
-  py::module_ module_matrix =
-      m.def_submodule("matrix", "Submodule for Ginkgos matrix type bindings");
+	py::module_ module_matrix =
+		m.def_submodule("matrix", "Submodule for Ginkgos matrix type bindings");
 
-  init_dense(module_matrix);
-  init_coo(module_matrix);
-  init_csr(module_matrix);
+	init_dense(module_matrix);
+	init_coo(module_matrix);
+	init_csr(module_matrix);
 }
