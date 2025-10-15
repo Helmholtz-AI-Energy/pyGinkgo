c0acfd5ff441961eeb80d8753c95283b7fc1d217
Gregor Olenik <gregor.olenik@web.de>
2025-10-15 17:44:47 +0200

    wip distributed bindings

--- src/cpp_bindings/distributed/matrix.cpp
+++ src/cpp_bindings/distributed/matrix.cpp
@@ -1,3 +1,30 @@
 // SPDX-FileCopyrightText: 2025 pyGinkgo authors
 //
 // SPDX-License-Identifier: MIT
+
+#include "../python.hpp"
+#include "../utils.hpp"
+
+
+template <typename ValueType, typename IndexType>
+void init_distributed_matrix(py::module_ &module, const std::string value_type,
+                             const std::string index_type)
+{
+    std::string matrix_type_repr = "pygko.dist_matrix.object";
+    std::string value_index_str = value_type + "_" + index_type;
+    std::string pyclass_name = matrix_type_repr + "_" + value_index_str;
+
+    using Matrix = gko::experimental::distributed::Matrix<ValueType, IndexType>;
+    py::class_<Matrix, std::shared_ptr<Matrix>, gko::LinOp>(
+        module, pyclass_name.c_str());
+}
+
+void init_dist_matrix_all_types(py::module_ &module)
+{
+#define DECLARE_DISTRIBUTED_MATRIX_VALUE_INDEX(ValueType, IndexType)  \
+    init_distributed_matrix<ValueType, IndexType>(module, #ValueType, \
+                                                  #IndexType);
+
+    PYGKO_INSTANTIATE_FOR_EACH_NON_COMPLEX_VALUE_AND_INDEX_TYPE(
+        DECLARE_DISTRIBUTED_MATRIX_VALUE_INDEX);
+}
