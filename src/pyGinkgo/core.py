a3d617096ebcebc3480233b87ac42bd43eef346c
Kai-Hui You <nana39193@gmail.com>
2026-06-30 09:29:39 +0200

    docs: correct solver_args criteria example

--- src/pyGinkgo/core.py
+++ src/pyGinkgo/core.py
@@ -157,7 +157,7 @@ def generate_solver(A, solver_args: Optional[dict] = None):
     Parameters: A - The system matrix
                 solver_args - An optional dictionary containing 
                     arguments forwarded to the solver, 
-                    for example: {"type": "solver::Cg", "criteria": {"max_iters": 100}}.
+                    for example: {"type": "solver::Cg", "criteria": [{"type": "Iteration", "max_iters": 100}]}.
     Returns: the solver
     """
 
@@ -231,7 +231,7 @@ def solve(A, b, initial_guess=None, solver_args: Optional[dict] = None, kind="co
                 initial_guess - The initial guess
                 solver - The solver
                 solver_args - A dictionary that is forwarded to the solver containing
-                    arguments, eg {'type': 'solver::Cg', 'criteria': {'max_iters': 100}}
+                    arguments, eg {'type': 'solver::Cg', 'criteria': [{"type": "Iteration", "max_iters": 100}]}
                 kind - the underlying solver, eg. config
     Returns: tuple of a logger object and solution vector
     """
